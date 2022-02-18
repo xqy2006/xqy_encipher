fca3b4cf66e19fe531184e8c72f3232a2c9308cc
xqy2006 <48169260+xqy2006@users.noreply.github.com>
2022-02-18 13:00:51 +0800

    Add files via upload

--- encipher.py
+++ encipher.py
@@ -30,7 +30,7 @@ def realcode1(dir,mima,mima_len,i,num,q):
     file_stats = os.stat(dir)
     max = int(file_stats.st_size)
     while True:
-        if i+1024>=(max*num)//4:
+        if i+1024>=(min(max,10000000)*num)//4:
             q.put('done')
             break
         a=int(read(dir,i,mima_len//2+1),16)
@@ -49,9 +49,9 @@ def coding(dir,mima,mima_len):
     max = int(file_stats.st_size)
     q = Queue()
     p1 = Process(target=realcode1, args=(dir,mima,mima_len,0,1,q,))
-    p2 = Process(target=realcode1, args=(dir,mima,mima_len,max//4,2,q,))
-    p3 = Process(target=realcode1, args=(dir,mima,mima_len,(max*2)//4,3,q,))
-    p4 = Process(target=realcode1, args=(dir,mima,mima_len,(max*3)//4,4,q,))
+    p2 = Process(target=realcode1, args=(dir,mima,mima_len,min(max,10000000)//4,2,q,))
+    p3 = Process(target=realcode1, args=(dir,mima,mima_len,(min(max,10000000)*2)//4,3,q,))
+    p4 = Process(target=realcode1, args=(dir,mima,mima_len,(min(max,10000000)*3)//4,4,q,))
     p1.start()
     p2.start()
     p3.start()
@@ -70,6 +70,10 @@ def coding(dir,mima,mima_len):
     p2.join()
     p3.join()
     p4.join()
+    p1.terminate()
+    p2.terminate()
+    p3.terminate()
+    p4.terminate()
 def jiami(dir,save_dir,mima,mima_len):
     shutil.copyfile(dir,save_dir)
     coding(save_dir,mima,mima_len)
@@ -95,7 +99,7 @@ def function3():#jiami
     save_dir = filedialog.asksaveasfilename(initialfile=file_path1+'.xqy',filetypes=[('XUQINYANG FILES','.xqy')])
     if save_dir!='':
         file_stats = os.stat(file_path1)
-        bar1['maximum'] = int(file_stats.st_size)+1
+        bar1['maximum'] = min(int(file_stats.st_size),10000000)+1
         jiami(file_path1,save_dir,mima,mima_len)
         bar1['value'] = 0
         tkinter.messagebox.showinfo('加密','加密完成，保存路径为'+save_dir)
@@ -106,7 +110,7 @@ def function4():#jiemi
     save_dir = filedialog.asksaveasfilename(initialfile=file_path2[:file_path2.rfind('.')])
     if save_dir!='':
         file_stats = os.stat(file_path2)
-        bar1['maximum'] = int(file_stats.st_size)+1
+        bar1['maximum'] = min(int(file_stats.st_size),10000000)+1
         jiemi(file_path2,save_dir,mima,mima_len)
         bar1['value'] = 0
         tkinter.messagebox.showinfo('解密','解密完成，保存路径为'+save_dir)
